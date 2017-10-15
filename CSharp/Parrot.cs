5b208357299a395351e944d4075fae7cdbb4067d
Ernst Perpignand <perpigne@gmail.com>
2017-10-15 12:20:01 -0400

    introduced strategy for speed calculation moved speed calculation to new object

--- CSharp/Parrot.cs
+++ CSharp/Parrot.cs
@@ -4,47 +4,57 @@ namespace parrot
 {
     public class Parrot
     {
-        readonly ParrotTypeEnum _type;
-        readonly int _numberOfCoconuts;
-        readonly double _voltage;
-        readonly bool _isNailed;
-
         public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed)
         {
-            _type = type;
-            _numberOfCoconuts = numberOfCoconuts;
-            _voltage = voltage;
-            _isNailed = isNailed; 
+            Strategy = new SpeedStrategy();
+            Type = type;
+            NumberOfCoconuts = numberOfCoconuts;
+            Voltage = voltage;
+            IsNailed = isNailed; 
         }
 
-        public double GetSpeed()
-        {
-            switch (_type)
-            {
-                case ParrotTypeEnum.EUROPEAN:
-                    return GetBaseSpeed();
-                case ParrotTypeEnum.AFRICAN:
-                    return Math.Max(0, GetBaseSpeed() - GetLoadFactor() * _numberOfCoconuts);
-                case ParrotTypeEnum.NORWEGIAN_BLUE:
-                    return (_isNailed) ? 0 : GetBaseSpeed(_voltage);
-            }
+        public SpeedStrategy Strategy { get; }
 
-            throw new Exception("Should be unreachable");
-        }
+        public ParrotTypeEnum Type { get; }
 
-        private double GetBaseSpeed(double voltage)
+        public int NumberOfCoconuts { get; }
+
+        public bool IsNailed { get; }
+
+        public double Voltage { get; }
+
+        public double GetBaseSpeed(double voltage)
         {
             return Math.Min(24.0, voltage * GetBaseSpeed());
         }
 
-        private double GetLoadFactor()
+        public double GetLoadFactor()
         {
             return 9.0;
         }
 
-        private double GetBaseSpeed()
+        public double GetBaseSpeed()
         {
             return 12.0;
+        }
+    }
+
+    public class SpeedStrategy
+    {
+        public double GetSpeed(Parrot parrot)
+        {
+
+            switch (parrot.Type)
+            {
+                case ParrotTypeEnum.EUROPEAN:
+                    return parrot.GetBaseSpeed();
+                case ParrotTypeEnum.AFRICAN:
+                    return Math.Max(0, parrot.GetBaseSpeed() - parrot.GetLoadFactor() * parrot.NumberOfCoconuts);
+                case ParrotTypeEnum.NORWEGIAN_BLUE:
+                    return (parrot.IsNailed) ? 0 : parrot.GetBaseSpeed(parrot.Voltage);
+            }
+
+            throw new Exception("Should be unreachable");
         }
     }
 }
--- CSharp/ParrotTest.cs
+++ CSharp/ParrotTest.cs
@@ -9,49 +9,49 @@ namespace parrot
         public void GetSpeedOfEuropeanParrot()
         {
             Parrot parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
-            Assert.AreEqual(12.0, parrot.GetSpeed());
+            Assert.AreEqual(12.0, parrot.Strategy.GetSpeed(parrot));
         }
 
         [TestMethod]
         public void GetSpeedOfAfricanParrot_With_One_Coconut()
         {
             Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
-            Assert.AreEqual(3.0, parrot.GetSpeed());
+            Assert.AreEqual(3.0, parrot.Strategy.GetSpeed(parrot));
         }
 
         [TestMethod]
         public void GetSpeedOfAfricanParrot_With_Two_Coconuts()
         {
             Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
-            Assert.AreEqual(0.0, parrot.GetSpeed());
+            Assert.AreEqual(0.0, parrot.Strategy.GetSpeed(parrot));
         }
 
         [TestMethod]
         public void GetSpeedOfAfricanParrot_With_No_Coconuts()
         {
             Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 0, 0, false);
-            Assert.AreEqual(12.0, parrot.GetSpeed());
+            Assert.AreEqual(12.0, parrot.Strategy.GetSpeed(parrot));
         }
 
         [TestMethod]
         public void GetSpeedNorwegianBlueParrot_nailed()
         {
             Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, true);
-            Assert.AreEqual(0.0, parrot.GetSpeed());
+            Assert.AreEqual(0.0, parrot.Strategy.GetSpeed(parrot));
         }
 
         [TestMethod]
         public void GetSpeedNorwegianBlueParrot_not_nailed()
         {
             Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, false);
-            Assert.AreEqual(18.0, parrot.GetSpeed());
+            Assert.AreEqual(18.0, parrot.Strategy.GetSpeed(parrot));
         }
 
         [TestMethod]
         public void GetSpeedNorwegianBlueParrot_not_nailed_high_voltage()
         {
             Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
-            Assert.AreEqual(24.0, parrot.GetSpeed());
+            Assert.AreEqual(24.0, parrot.Strategy.GetSpeed(parrot));
         }
     }
 }
