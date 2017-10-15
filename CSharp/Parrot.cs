1b7cf62819ec03459121a4119f13632ea617d630
Ernst Perpignand <perpigne@gmail.com>
2017-10-14 21:53:19 -0400

    introduced other Parrot Classes

--- CSharp/Parrot.cs
+++ CSharp/Parrot.cs
@@ -1,45 +1,14 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace parrot
 {
-    public class Parrot
+    public abstract class Parrot
     {
-        private const double LoadFactor = 9.0;
-        private const double BaseSpeed = 12.0;
-        private const double MaxSpeed = 24.0;
+        protected const double BaseSpeed = 12.0;
 
-        readonly ParrotTypeEnum _type;
-        readonly int _numberOfCoconuts;
-        readonly double _voltage;
-        readonly bool _isNailed;
 
-        internal Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed)
-        {
-            _type = type;
-            _numberOfCoconuts = numberOfCoconuts;
-            _voltage = voltage;
-            _isNailed = isNailed; 
-        }
-
-        protected Parrot()
-        {
-            
-        }
-
-        public double GetSpeed()
-        {
-            switch (_type)
-            {
-                case ParrotTypeEnum.EUROPEAN:
-                    return BaseSpeed;
-                case ParrotTypeEnum.AFRICAN:
-                    return Math.Max(0, BaseSpeed - LoadFactor * _numberOfCoconuts);
-                case ParrotTypeEnum.NORWEGIAN_BLUE:
-                    return (_isNailed) ? 0 : Math.Min(MaxSpeed, _voltage * BaseSpeed);
-            }
-
-            throw new Exception("Should be unreachable");
-        }
+        public abstract double GetSpeed();
 
         public static Parrot Create(ParrotTypeEnum type, int numberOfCoconuts, double voltage, bool isNailed)
         {
@@ -47,13 +16,54 @@ namespace parrot
             {
                 case ParrotTypeEnum.EUROPEAN:
                     return new EuropeanParrot();
-                default:
-                    return new Parrot(type, numberOfCoconuts, voltage, isNailed);
+                case ParrotTypeEnum.AFRICAN:
+                    return new AfricanParrot(numberOfCoconuts);
+                case ParrotTypeEnum.NORWEGIAN_BLUE:
+                    return new NorwegianBlueParrot(voltage, isNailed);
             }
+            throw new Exception("missing case for type " + type);
+        }
+    }
+
+    public class NorwegianBlueParrot : Parrot
+    {
+        protected const double MaxSpeed = 24.0;
+        private readonly double _voltage;
+        private readonly bool _isNailed;
+
+        public NorwegianBlueParrot(double voltage, bool isNailed)
+        {
+            _voltage = voltage;
+            _isNailed = isNailed;
+        }
+
+        public override double GetSpeed()
+        {
+            return (_isNailed) ? 0 : Math.Min(MaxSpeed, _voltage * BaseSpeed);
+        }
+    }
+
+    public class AfricanParrot : Parrot
+    {
+        protected const double LoadFactor = 9.0;
+        private readonly int _numberOfCoconuts;
+
+        public AfricanParrot(int numberOfCoconuts)
+        {
+            this._numberOfCoconuts = numberOfCoconuts;
+        }
+
+        public override double GetSpeed()
+        {
+            return Math.Max(0, BaseSpeed - LoadFactor * _numberOfCoconuts);
         }
     }
 
     public class EuropeanParrot : Parrot
     {
+        public override double GetSpeed()
+        {
+           return BaseSpeed;
+        }
     }
 }
